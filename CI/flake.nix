4100cfe610696c2c413044e33b37d026535afa4c
Github Action <gha@cardanofoundation.org>
2025-02-04 08:23:30 +0000

    Smoke test linux package

--- CI/flake.nix
+++ CI/flake.nix
@@ -1,29 +1,19 @@
 {
   description = "Cardano Deposit Wallet CI";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
-  };
+  inputs = { nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11"; };
 
-  outputs =
-    inputs@{ flake-parts, nixpkgs, ... }:
+  outputs = inputs@{ flake-parts, nixpkgs, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       imports = [ ];
-      systems = [
-        "x86_64-linux"
-        "aarch64-darwin"
-        "x86_64-darwin"
-      ];
+      systems = [ "x86_64-linux" "aarch64-darwin" "x86_64-darwin" ];
       # https://flake.parts/index.html
-      perSystem =
-        { system, ... }:
-        {
-          devShells.docs =
-            with import nixpkgs { inherit system; };
-            mkShell {
-              packages = [ mdbook tree gnused ];
-            };
-        };
+      perSystem = { system, ... }: {
+        devShells.docs = with import nixpkgs { inherit system; };
+          mkShell { packages = [ gnused gnutar jq mdbook tree wget ]; };
+        devShells.tests = with import nixpkgs { inherit system; };
+          mkShell { packages = [ gnutar jq screen wget ]; };
+      };
 
     };
 
